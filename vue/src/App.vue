ee12326859e0c52d9239c5bfcf160b83fe6669f4
Daniel Husik <dhusik11@gmail.com>
2023-08-07 21:43:16 -0400

    logged in user removes "Login/Register", Home UI

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,29 @@
 <template>
+
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'login' }">Login/Register</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'volunteer' }">Volunteer</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'support' }">Support</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <!-- <img src="src/asset/logo.png"> -->
+      <router-link class="navlink" v-bind:to="{ name: 'home' }">|  Home</router-link>&nbsp;|&nbsp;
+      <router-link class="navlink" v-if="$store.state.token === ''" to="/login">Login/Register  |  </router-link>
+      <router-link class="navlink" v-bind:to="{ name: 'volunteer' }">Volunteer</router-link>&nbsp;|&nbsp;
+      <router-link class="navlink" v-bind:to="{ name: 'support' }">Support</router-link>&nbsp;|&nbsp;
+      <router-link class="navlink" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+
+<style scoped>
+#nav {
+  display:flex;
+  justify-content: center;
+  padding: 15px;
+	background: #d16602;
+}
+#nav > a {
+  color: black;  
+  font-size: 20px;
+  text-decoration: none;
+}
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -10,3 +10,23 @@ export default {
   name: "home"
 };
 </script>
+
+
+<style scoped>
+#app > div.home {
+	justify-content: center;
+  padding-top:5px;
+	align-items: center;
+	min-height: 100vh;
+	font-family: 'Jost', sans-serif;
+	background: linear-gradient(to bottom, #0f0c29, #302b63, #24243e);
+}
+h1 {
+  color: white;
+  text-align: center;
+}
+p {
+  color: white;
+  text-align: center;
+}
+</style>
